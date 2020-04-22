643ac525868901fbf45ae594bdc990bc816cbfc9
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-22 15:59:49 +0900

    ABC評価機能の追加

---
+++ src/IfSample.java
@@ -0,0 +1,18 @@
+
+public class IfSample {
+
+	public static void main(String[] args) {
+		System.out.println("点数を入力してください。");
+		int num = new java.util.Scanner(System.in).nextInt();
+		if (num >= 80) {
+			System.out.println("A評価となります");
+		}
+		else if (num >= 60) {
+			System.out.println("B評価となります");
+		}
+		else {
+			System.out.println("C評価となります");
+		}
+	}
+
+}
