2a1d8fd1857701851190077021269d6b9f04bd88
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-22 13:27:36 +0900

    New3.14

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		double p = 3.14;
-		System.out.println("円周率は"+p+"です。");
+		double p = 13.14;
+		System.out.println("円周率は"+p+"ではないです。");
 	}
 }
