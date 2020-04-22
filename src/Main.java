353e4d645db4c91601782313de8dabc374f690db
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-22 14:18:18 +0900

    Heikin

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,34 @@
 public class Main {
 
 	public static void main(String[] args) {
-		double p = 13.14;
-		System.out.println("円周率は"+p+"ではないです。");
+		double p = 3.14;
+		System.out.println("円周率は"+p+"です。");
+		
+		int a;
+		int b;
+		a = 20;
+		b = a+5;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("今日は\n\"4月\"です。");
+		System.out.println("\\100");
+		System.out.println("C:\\に\"フォルダ\"を作成");
+		
+		int num = 10;
+		int amari = 10 % 3;//　10/3のあまり
+		System.out.println(amari);
+		
+		num += 1;
+		num ++;
+		System.out.println(num);
+		
+		double d = 3;
+		int n = (int)3.0;
+		String s = "べすと" + 3;
+		
+		double kokugo = 70;
+		double sansu = 75;
+		double heikin = (kokugo + sansu) /2;
+		System.out.println(heikin);
 	}
 }
