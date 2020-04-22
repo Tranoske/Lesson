459232036a88f4b18672c11c3f4cb64a3d02bf00
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-22 15:09:36 +0900

    足し算ができちゃう新機能を追加。

---
+++ src/Keisan.java
@@ -0,0 +1,15 @@
+
+public class Keisan {
+	public static void main(String[] args) {
+		/*
+		System.out.println("数をにゅーりょく");
+		int num =new java.util.Scanner(System.in).nextInt();
+		System.out.println(num*2);
+		*/
+		System.out.println("□+nをにゅーりょく");
+		int input1 = new java.util.Scanner(System.in).nextInt();
+		System.out.println(input1+"+□をにゅーりょく");
+		int input2 = new java.util.Scanner(System.in).nextInt();
+		System.out.println("答えは"+(input1 + input2)+"やね。");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		/*
 		double p = 3.14;
 		System.out.println("円周率は"+p+"です。");
-		
+
 		int a;
 		int b;
 		a = 20;
@@ -15,26 +15,45 @@ public class Main {
 		System.out.println("今日は\n\"4月\"です。");
 		System.out.println("\\100");
 		System.out.println("C:\\に\"フォルダ\"を作成");
-		
+
 		int num = 10;
 		int amari = 10 % 3;//　10/3のあまり
 		System.out.println(amari);
-		
+
 		num += 1;
 		num ++;
 		System.out.println(num);
-		
+
 		double d = 3;
 		int n = (int)3.0;
 		String s = "べすと" + 3;
-		
+
 		int kokugo = 70;
 		int sansu = 75;
 		double heikin = (kokugo + sansu) /2.0;
 		System.out.println(heikin);
-		*/
+		
+
 		int num1 = 3;
 		int num2 = 5;
 		System.out.println("足し算の結果は"+(num1+num2));
+		System.out.print("私は");
+		System.out.print("maoli huzhijie");
+		System.out.println("です");
+		
+		int m = Math.max(10, 20);
+		System.out.println(m);
+		
+		String age = "22";
+		int nAge = Integer.parseInt(age);
+		System.out.println(nAge+1);
+		
+		int r = new java.util.Random().nextInt(101);
+		System.out.println(r);
+		*/
+		
+		//サイコロ１～６
+		int saikoro = new java.util.Random().nextInt(6)+1;
+		System.out.println(saikoro);
 	}
 }
