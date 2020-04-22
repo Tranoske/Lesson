523f0526da4d33e24211a511c19920994f8aae6a
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-22 16:22:35 +0900

    First.九九計算問題

---
+++ src/Kuku.java
@@ -0,0 +1,21 @@
+
+public class Kuku {
+
+	public static void main(String[] args) {
+		int uhen = new java.util.Random().nextInt(10);
+		int sahen = new java.util.Random().nextInt(10);
+		int ans = uhen*sahen;
+		System.out.println(uhen+"×"+sahen+"＝？？？");
+		
+		int input = new java.util.Scanner(System.in).nextInt();
+		
+		if (input == ans ) {
+			System.out.println("正解！");
+		}
+		else {
+			System.out.println("は？違う。");
+			System.out.println(uhen+"×"+sahen+"＝"+ans);
+		}
+	}
+
+}
