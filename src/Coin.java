d67c7c8a41114edf2098cc531dd5741e2d1216ce
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-23 16:46:34 +0900

    maisuのlengthをcoinsのに変更

--- src/Coin.java
+++ src/Coin.java
@@ -3,7 +3,7 @@ public class Coin {
 
 	public static void main(String[] args) {
 		int[] coins = {500,100,50,10,5,1};
-		int[] maisu = new int [6];
+		int[] maisu = new int [coins.length];
 		for(int i=0;i<coins.length;i++) {
 			System.out.println(coins[i]+"円玉は何枚？");
 			maisu[i] = new java.util.Scanner(System.in).nextInt();
