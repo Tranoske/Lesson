8d2c92f23624f064c29a3d5d1398facbfc5f630c
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-24 13:54:32 +0900

    拡張for、メソッド化

--- src/Coin.java
+++ src/Coin.java
@@ -3,19 +3,17 @@ public class Coin {
 
 	public static void main(String[] args) {
 		int[] coins = {500,100,50,10,5,1};
-		int[] maisu = new int [coins.length];
-		for(int i=0;i<coins.length;i++) {
-			System.out.println(coins[i]+"円玉は何枚？");
-			maisu[i] = new java.util.Scanner(System.in).nextInt();
+		int sum=0;
+		for (int coin:coins) {
+			int maisu = inputMaisu(coin);
+			sum += coin*maisu;
 		}
-		int[] coinsums = new int[6];
-		for (int i=0;i<coins.length;i++){
-			coinsums[i] = maisu[i]*coins[i];
-		}
-		int sum = 0;
-		for(int value:coinsums) {
-			sum += value;
-		}
-		System.out.println(sum+"円");
+		System.out.println("合計："+sum);
+		
+	}
+	public static int inputMaisu(int coin) {
+		System.out.println(coin+"円玉は何枚？");
+		int maisu = new java.util.Scanner(System.in).nextInt();
+		return maisu;
 	}
 }
