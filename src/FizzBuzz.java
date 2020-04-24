6f01fbd53d66ff2b8f7428d47949ed69f874b85e
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-24 15:15:39 +0900

    メソッド化

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,23 +3,20 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		for(int i=1;i<=30;i++) {
-			showFizzBuzz(i);
+			System.out.println(getFizzBuzz(i));
 		}
 	}
-	public static void showFizzBuzz(int num) {
+	public static String getFizzBuzz(int num) {
 		if(num % 3 == 0 && num % 5 == 0) {
-			System.out.print(num);
-			System.out.println("FizzBuzz");
+			return num+"FizzBuzz";
 		}
 		else if(num % 3 ==0) {
-			System.out.print(num);
-			System.out.println("Fizz");
+			return num+"Fizz";
 		}
 		else if(num % 5 ==0) {
-			System.out.print(num);
-			System.out.println("Buzz");
+			return num+"Buzz";
 		}
 		else
-			System.out.println(num);
+			return num+"";
 	}
 }
