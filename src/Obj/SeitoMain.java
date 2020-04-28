61c53eb348873b93e8c3b70491381e2bdd8f630b
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-28 17:02:25 +0900

    Array化

--- src/Obj/SeitoMain.java
+++ src/Obj/SeitoMain.java
@@ -1,24 +1,21 @@
 package Obj;
 
+import java.util.ArrayList;
+
 public class SeitoMain {
 
 	public static void main(String[] args) {
-		Chugakusei tnk = new Chugakusei("田中",80,70,50,9);
-		tnk.show();
-		System.out.println("合計:"+tnk.goukei()+"点");
-		System.out.println("平均:"+tnk.heikin()+"点");
-		System.out.println();
-		Chugakusei ymd = new Chugakusei("山田",20,70,40,90);
-		ymd.show();
-		System.out.println("合計:"+ymd.goukei()+"点");
-		System.out.println("平均:"+ymd.heikin()+"点");
-		System.out.println();
-		Chugakusei mri = new Chugakusei("毛利",0,0,0,200);
-		mri.show();
-		System.out.println("合計:"+mri.goukei()+"点");
-		System.out.println("平均:"+mri.heikin()+"点");
+		int total = 0;
+		ArrayList<Chugakusei> cs = new ArrayList<Chugakusei>();
+		cs.add(new Chugakusei("田中",80,70,50,9));
+		cs.add(new Chugakusei("山田",20,70,40,90));
+		cs.add(new Chugakusei("毛利",0,0,0,200));
 		
-		System.out.println();
-		System.out.println("全員の合計:"+(tnk.goukei()+ymd.goukei()+mri.goukei())+"点");
+		for(Chugakusei c:cs) {
+			c.show();
+			total += c.goukei();
+		}
+		
+		System.out.println("全員の合計は"+total);
 	}
 }
