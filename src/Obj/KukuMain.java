4b8721df76f2ec6e7f8cfedfb78be5ae8722a11e
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-04-28 16:11:07 +0900

    ArrayListKuku

---
+++ src/Obj/KukuMain.java
@@ -0,0 +1,22 @@
+package Obj;
+
+import java.util.ArrayList;
+
+public class KukuMain {
+
+	public static void main(String[] args) {
+		ArrayList<KukuMondai> kms = new ArrayList<KukuMondai>();
+		kms.add(new KukuMondai());
+		kms.add(new KukuMondai());
+		kms.add(new KukuMondai());
+		kms.add(new KukuMondai());
+		kms.add(new KukuMondai());
+		
+		for(KukuMondai km:kms) {
+			km.show();
+			int ans = new java.util.Scanner(System.in).nextInt();
+			km.check(ans);
+		}
+	}
+
+}
---
+++ src/Obj/KukuMondai.java
@@ -0,0 +1,25 @@
+package Obj;
+
+public class KukuMondai {
+	int num1;
+	int num2;
+	
+	public KukuMondai() {
+		num1 = new java.util.Random().nextInt(9)+1;
+		num2 = new java.util.Random().nextInt(9)+1;
+	}
+	
+	public void show() {
+		System.out.print(num1+"×"+num2+"＝");
+	}
+	public void check(int kotae) {
+		if (kotae == num1*num2) {
+			System.out.println("せいかい！");
+		}else {
+			System.out.println("は？違う。");
+			System.out.println(kotae);
+		}
+	}
+	
+	
+}
