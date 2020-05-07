b9e29dd3dd63c27a15deb0084479d672f6eb3a66
edu05 <edu05@EDUPC05.n00d01.kis.co.jp>
2020-05-07 16:20:07 +0900

    合計を忘れていた

--- src/rental/RentalMain.java
+++ src/rental/RentalMain.java
@@ -10,10 +10,12 @@ public class RentalMain {
 		items.add(new CD("パプリ力",3));
 		items.add(new CD("Lemonn",1));
 		
+		int sum = 0;
 		for(Item i:items) {
 			System.out.println(i.name+"  "+i.days+"日 "+i.getPrice()+"円");
-		
+			sum += i.getPrice();
 		}
+		System.out.println("合計："+sum+"円");
 
 	}
 
